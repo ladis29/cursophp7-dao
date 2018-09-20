e4ecd4a9ec903e3e844b93dd3cbaa3acedba24d7
Ladislau Felisbino <ladis29@gmail.com>
2018-09-20 12:34:41 -0300

    Criada a classe usuário

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php 
 
 	require_once("config.php");
-
+/*
 	$sql = new Sql();
 
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 	echo json_encode($usuarios);
+*/
+
+	$user = new Usuario();
+
+	$user->loadById(3);
+
+	echo $user;
 
  ?>
