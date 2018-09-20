6fc806a0c23d1c5afbbcf4423f772d852e5a0a37
Ladislau Felisbino <ladis29@gmail.com>
2018-09-20 16:09:08 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -7,12 +7,29 @@
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 	echo json_encode($usuarios);
-*/
 
+// carrega um usuário
 	$user = new Usuario();
 
 	$user->loadById(3);
 
 	echo $user;
+
+//carrega uma lista de usuários
+	$lista = Usuario::getList();
+
+	echo json_encode($lista);
+
+//carrega uma lista de usuários buscando pelo login
+	$search = Usuario::search("jo");
+
+	echo json_encode($search);
+*/
+
+//carrega um usuário usando o login e a senha
+	$usuario = new Usuario();
+	$usuario->login("joao", "qwerty");
+
+	echo $usuario;
 
  ?>
